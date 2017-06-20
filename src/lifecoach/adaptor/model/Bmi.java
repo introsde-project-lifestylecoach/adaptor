b3ea3bc6c049d0a1cec156e837654dd780e60216
ABrun93 <a.brun93@gmail.com>
2017-06-20 10:02:34 +0200

    Set the response with Bmi class

--- src/lifecoach/adaptor/model/Bmi.java
+++ src/lifecoach/adaptor/model/Bmi.java
@@ -2,17 +2,60 @@ package lifecoach.adaptor.model;
 
 public class Bmi 
 {
-	char sex;
-	int age;
-	float weight, height, waist, hip;
+	/*
+	 * Basal metabolic rate (BMR): is the total number of calories that your body needs to perform basic, life-sustaining functions.
+	 * Waist to hip ratio (WHR): is a measurement that compares the size of your waist in inches to the size of your hips in inches. 
+	 * 							 Risk for developing heart disease can be estimated using your waist to hip ratio
+	 */
 	
-	public Bmi(char sex, int age, float weight, float height, float waist, float hip) {
-		super();
-		this.sex = sex;
-		this.age = age;
-		this.weight = weight;
-		this.height = height;
-		this.waist = waist;
-		this.hip = hip;
+	double bmi_value, bmr_value;
+    String bmi_status, bmi_risk, ideal_weight, whr_status;
+	
+    public double getBmi_value() {
+		return bmi_value;
 	}
+	
+    public void setBmi_value(double bmi_value) {
+		this.bmi_value = bmi_value;
+	}
+		
+    public String getBmi_status() {
+		return bmi_status;
+	}
+	
+    public void setBmi_status(String bmi_status) {
+		this.bmi_status = bmi_status;
+	}
+	
+    public String getBmi_risk() {
+		return bmi_risk;
+	}
+	
+    public void setBmi_risk(String bmi_risk) {
+		this.bmi_risk = bmi_risk;
+	}
+	
+    public String getIdeal_weight() {
+		return ideal_weight;
+	}
+	
+    public void setIdeal_weight(String ideal_weight) {
+		this.ideal_weight = ideal_weight;
+	}
+	
+    public double getBmr_value() {
+		return bmr_value;
+	}
+	
+    public void setBmr_value(double bmr_value) {
+		this.bmr_value = bmr_value;
+	}
+    
+    public String getWhr_status() {
+		return whr_status;
+	}
+	
+    public void setWhr_status(String whr_status) {
+		this.whr_status = whr_status;
+	}    
 }
--- src/lifecoach/adaptor/webservice/AdaptorImplementation.java
+++ src/lifecoach/adaptor/webservice/AdaptorImplementation.java
@@ -25,6 +25,7 @@ public class AdaptorImplementation implements Adaptor
     {    	
         int status;
 		String body, result;
+		Bmi bmi = new Bmi();
 		
 		System.out.println("Calcolate Bmi with: weight=" + weight + ", height=" + height + ", sex=" + sex
        		 + ", age=" + age + ", waist=" + waist + ", hip=" + hip);
@@ -45,7 +46,7 @@ public class AdaptorImplementation implements Adaptor
 		} 
 		catch (UnirestException e) 
 		{
-			// TODO Auto-generated catch block
+			// TODO Handle the exception
 			e.printStackTrace();
 		}
 		
@@ -56,14 +57,21 @@ public class AdaptorImplementation implements Adaptor
 			result = (status == 200 || status == 201 || status == 202 ? "OK" : "ERROR");		
 			System.out.println("--> Result: " + result);
 			
+			
 			body = response.getBody().toString();
 			System.out.println(body);
-			
 			JSONObject jsonObject = new JSONObject(body);
+			bmi.setIdeal_weight(jsonObject.getString("ideal_weight"));
 	        JSONObject jsonBmi = jsonObject.getJSONObject("bmi");
-	        System.out.println(jsonBmi.toString());
-	        System.out.println(jsonBmi.getString("risk"));
-	        System.out.println(jsonBmi.getString("status"));
+	        bmi.setBmi_value(jsonBmi.getDouble("value"));
+	        bmi.setBmi_status(jsonBmi.getString("status"));
+	        bmi.setBmi_risk(jsonBmi.getString("risk"));
+	        JSONObject jsonBmr = jsonObject.getJSONObject("bmr");
+	        bmi.setBmr_value(jsonBmr.getDouble("value"));
+	        JSONObject jsonWhr = jsonObject.getJSONObject("whr");
+	        bmi.setWhr_status(jsonWhr.getString("status"));
+	        
+	        return bmi;
 		}
 		
         return null;
